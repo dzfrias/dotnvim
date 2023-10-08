5c0aa2c12ac9e8e3eccc43bde920987fec686216
Diego <96022404+dzfrias@users.noreply.github.com>
2023-10-07 19:50:17 -0700

    change some plugins

--- lua/dzfrias/plugins/git.lua
+++ lua/dzfrias/plugins/git.lua
@@ -47,10 +47,7 @@ return {
   },
 
   {
-    'kdheepak/lazygit.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    keys = { { '<leader>g', '<Cmd>LazyGit<CR>', desc = 'Open LazyGit' } },
+    'tpope/vim-fugitive',
+    keys = { { '<leader>g', '<Cmd>Git<CR>', desc = 'Open fugitive' } },
   },
 }
--- lua/dzfrias/plugins/visuals.lua
+++ lua/dzfrias/plugins/visuals.lua
@@ -3,6 +3,7 @@ return {
   {
     'dzfrias/noir.nvim',
     lazy = false,
+    dev = true,
     priority = 1000,
     config = function()
       vim.cmd [[colorscheme noir]]
