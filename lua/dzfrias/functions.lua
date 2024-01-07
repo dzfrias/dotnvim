ec14a39490a6b4762b64c149d44a7734559a9f6d
Diego <96022404+dzfrias@users.noreply.github.com>
2024-01-07 08:45:07 -0800

    Add cargo edit keybind

--- lua/dzfrias/functions.lua
+++ lua/dzfrias/functions.lua
@@ -25,12 +25,30 @@ end
 
 M.grep = function(vtype)
   if vtype == nil then
-    vim.go.operatorfunc = "v:lua.require'dzfrias.operators'.grep_callback"
+    vim.go.operatorfunc = "v:lua.require'dzfrias.functions'.grep_callback"
     vim.api.nvim_feedkeys('g@', 'n', false)
     return
   end
 
   M.grep_callback(vtype)
+end
+
+M.edit_cargo = function()
+  local cargo_paths = vim.fn.findfile('Cargo.toml', '.;', -1)
+  if #cargo_paths == 0 then
+    print 'No Cargo.toml found'
+    return
+  end
+  if #cargo_paths == 1 then
+    vim.cmd('edit ' .. cargo_paths[1])
+    return
+  end
+  vim.ui.select(cargo_paths, { prompt = 'Select Cargo.toml' }, function(choice)
+    if not choice then
+      return
+    end
+    vim.cmd('edit ' .. choice)
+  end)
 end
 
 return M
--- lua/dzfrias/keymaps.lua
+++ lua/dzfrias/keymaps.lua
@@ -1,5 +1,5 @@
 local util = require 'dzfrias.util'
-local operators = require 'dzfrias.operators'
+local functions = require 'dzfrias.functions'
 -- Set up map functions
 local nnoremap = util.nnoremap
 local inoremap = util.inoremap
@@ -16,12 +16,13 @@ noremap('k', 'gk', 'Move down')
 noremap('j', 'gj', 'Move up')
 
 nnoremap('<C-f>', '<C-^>', 'Alternate buffer')
-nnoremap('<space>p', operators.grep, 'Grep')
+nnoremap('<space>p', functions.grep, 'Grep')
 vnoremap(
   '<space>p',
-  ":<C-u>lua require('dzfrias.operators').grep(vim.fn.visualmode())<CR>",
+  ":<C-u>lua require('dzfrias.functions').grep(vim.fn.visualmode())<CR>",
   'Grep'
 )
+nnoremap('<leader>c', functions.edit_cargo, 'Edit Cargo.toml')
 
 -- Window resizing
 nnoremap('<C-s-l>', '<Cmd>vertical resize -5<CR>', 'Resize window left')
@@ -34,6 +35,5 @@ nnoremap('<s-CR>', 'O<Esc>', 'Make a newline upwards')
 
 -- Basic command mappings
 nnoremap('<leader>w', '<Cmd>write<CR>', 'Write file')
-nnoremap('<leader>q', '<Cmd>quit!<CR>', 'Quit file')
 
 inoremap('jk', '<Esc>', 'Escape')
