e25d99b8ddac718f267c806164ceaf06a71632bb
Diego <96022404+dzfrias@users.noreply.github.com>
2024-05-28 20:01:49 -0700

    Add harpoon 2

--- lua/dzfrias/plugins/coding.lua
+++ lua/dzfrias/plugins/coding.lua
@@ -44,18 +44,20 @@ return {
 
   {
     'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
       {
         '<leader>m',
         function()
-          require('harpoon.mark').add_file()
+          require('harpoon'):list():add()
         end,
       },
       {
         '<leader>o',
         function()
-          require('harpoon.ui').toggle_quick_menu()
+          local harpoon = require 'harpoon'
+          harpoon.ui:toggle_quick_menu(harpoon:list())
         end,
       },
     },
